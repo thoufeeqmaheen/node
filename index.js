fc7e7d3d9127c36378048f618f8238b5b08b3a7f
Afsal Khan <afsal@ionaught.com>
2022-12-19 15:04:23 +0530

    3

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const http = require("http");
  
 const server = http.createServer((req,res)=>{
+    console.log(req.method,req.url)
     res.statusCode = 200;
     res.setHeader("content-text","plane/test");
-    res.end("start programming");
+    if(req.url === ("/")){
+        res.end("home");
+    }
+    else if (req.url === "/about")
+{
+    res.end("about");
+}
+    
+    else
+    res.end("error");
+    
+    
 });
 
-server.listen(8000,"127.0.0.1",()=>{
-    console.log("server start at port ",8000)
+server.listen(5000,"127.0.0.1",()=>{
+    console.log("server start at port ",5000)
 });
